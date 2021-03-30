9fdf24180881c3cd979bf760790897ab9dee9e23
Karen <skarena20@gmail.com>
2021-03-30 13:37:48 -0500

    modificando localmente el archivo nav

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ol>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Quienes somos</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="#">Contacto</a></li>
+    </ol>
+</nav>
